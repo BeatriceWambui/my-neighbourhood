ae016c62372f509ce79288997ff112d41343a8d1
Wachira Beatrice <BeatriceWambui>
2019-10-28 11:34:02 +0300

    add color for h1 in the styles.css after creating a static folder

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    return render(request,'blueprint/index.html')
