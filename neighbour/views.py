5959caabfe3d252fd588e88e2b63a663d05bb22a
Wachira Beatrice <BeatriceWambui>
2019-10-28 14:56:12 +0300

    create views for signup

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/registration_form.html',{"form": form})
+
 @login_required(login_url='/accounts/login/')
 def index(request):
+    
     return render(request,'blueprint/index.html')
