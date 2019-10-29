04657070656e8c702543426c46d4c922ce3cf001
Wachira Beatrice <BeatriceWambui>
2019-10-29 16:34:25 +0300

    create view for profile import message and Profile from models

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import SignupForm
+from .forms import SignupForm,ProfileUpdateForm,UserUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
+from django.contrib import messages
 
 def signup(request):
     if request.method == "POST":
@@ -24,3 +26,31 @@ def signup(request):
 def index(request):
     
     return render(request,'blueprint/index.html')
+
+@login_required
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES, instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context={
+        'u_form': u_form,
+        'p_form': p_form,
+        'user':user
+    }
+
+    return render(request, 'blueprint/profile.html', context)
+
+def ProfileDetailView(LoginRequiredMixin, DetailView):
+   model = Profile
