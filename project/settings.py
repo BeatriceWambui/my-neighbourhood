f9f906f6e96669c2a01f80e61c593a843fc53c2a
Wachira Beatrice <BeatriceWambui>
2019-10-29 16:34:49 +0300

    create a template for profile to deiplay the profile image

--- project/settings.py
+++ project/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'neighbour.apps.NeighbourConfig',
+    'crispy_forms', 
     'bootstrap4',
     'bootstrap3',
     'django.contrib.admin',
@@ -109,10 +110,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
