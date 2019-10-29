2ff149d669e0510736d7c7731ee731a3254086bd
Wachira Beatrice <BeatriceWambui>
2019-10-29 16:31:45 +0300

    create profile update form to update profile

--- neighbour/forms.py
+++ neighbour/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile 
 
 
 def SignupForm(UserCreationForm):
@@ -9,3 +10,15 @@ def SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
