769c382b8e1e98113f1a7d046786394471f74fca
Wachira Beatrice <BeatriceWambui>
2019-10-29 22:49:25 +0300

    create urls for hood,user,business and search

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -6,4 +6,9 @@ urlpatterns=[
     url('^signup/$', views.signup, name='signup'),
     url('profile/', views.profile, name='profile'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^hoods/',views.neighbourhood,name='neighbourhood'),
+    url(r'^user/',views.user,name='user'),
+    url(r'^business/',views.user,name='business'),
+    url(r'^search/', views.search_results, name='search_results')
+
 ]
