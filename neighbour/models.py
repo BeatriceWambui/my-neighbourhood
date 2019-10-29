c0685ab60a432d89ef1c5e298af0990190eb98e9
Wachira Beatrice <BeatriceWambui>
2019-10-29 16:56:58 +0300

    create neighbourhood class in models.py

--- neighbour/models.py
+++ neighbour/models.py
@@ -34,3 +34,24 @@ class Profile(models.Model):
         return f'{self.user.username} Profile' 
     def save(self,*args, **kwargs):
         super().save(*args, **kwargs)
+
+class neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    occupants = models.IntegerField(default=0)
+    admin = models.ForeignKey(User,null=True)
+    
+    def create_neighbourhood(self):
+        self.save()
+
+    def delete_neighbourhood(self):
+        self.save()
+
+    def update_neighbourhood(self):
+        self.save()
+    
+    def update_occupants(self):
+        self.save()
+
+    def __str__(self):
+        return self.location
