d6746bd5660e49802e10570b5ac008a3f6f10d84
Wachira Beatrice <BeatriceWambui>
2019-10-30 13:25:21 +0300

    create a view for post in the views.py file

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .forms import SignupForm,ProfileUpdateForm,UserUpdateForm,NeighbourhoodForm,UserForm,BusinessForm
+from .forms import SignupForm,ProfileUpdateForm,UserUpdateForm,NeighbourhoodForm,UserForm,BusinessForm,PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile,Neighbourhood,User,Businesses
+from .models import Profile,Neighbourhood,User,Businesses,Post
 from django.contrib import messages
 
 def signup(request):
@@ -25,9 +25,8 @@ def signup(request):
 @login_required(login_url='/accounts/login/')
 def index(request):
     hood = Neighbourhood.objects.all()
-    users = User.objects.all()
-    business = Businesses.objects.all()
-    return render(request,'blueprint/index.html')
+    posts = Post.objects.all()
+    return render(request,'blueprint/index.html',{'hood':hood,'posts':posts})
 
 @login_required
 def profile(request):
@@ -58,46 +57,52 @@ def ProfileDetailView(LoginRequiredMixin, DetailView):
    model = Profile
 
 def neighbourhood(request):
+    current_user = request.user
+    business = Businesses.objects.all()
     if request.method == 'POST':
         form = NeighbourhoodForm(request.POST,request.FILES)
         if form.is_valid():
-            name = form.cleaned_data['name']
+            name=form.cleaned_data['name']
             location = form.cleaned_data['location']
-            occupation = form.cleaned_data['occupation']
-            current_user = request.user
-            saveHood = Neighbourhood(name=name,location=location,occupation=occupation)
+            occupants = form.cleaned_data['occupants']
+            saveHood = Neighbourhood(name=name,location=location,occupants=occupants)
             saveHood.save()
             return redirect(index)
     else:
         form = NeighbourhoodForm()
-    return render(request,'blueprint/upload.html',{'form':form})
+        business = Businesses.objects.all()
+    return render(request,'blueprint/upload.html',{'form':form,'business':business})
 
 def user(request):
+    current_user=request.user
     if request.method == 'POST':
+        user = current_user
         form = UserForm(request.POST,request.FILES)
         if form.is_valid():
-            usename = form.cleaned_data['username']
+            username = form.cleaned_data['username']
             user_id = form.cleaned_data['user_id']
             email = form.cleaned_data['email']
             saveUser = User(username=username,user_id=user_id,email=email)
             saveUser.save()
-            return redirect(index)
+            return redirect(neighbourhood)
     else:
+        users = User.objects.all()
         form = UserForm()
-    return render(request,'blueprint/users.html',{'form':form})
+    return render(request,'blueprint/users.html',{'form':form,'users':users})
+
 
 def business(request):
     if request.method == 'POST':
-        form = BusinessForm(request.POST,request.FILES)
-        if form.is_valid():
-            business_name = form.cleaned_data['business_name']
-            business_email = form.cleaned_data['business_email']
+        form2 = BusinessForm(request.POST,request.FILES)
+        if form2.is_valid():
+            business_name = form2.cleaned_data['business_name']
+            business_email = form2.cleaned_data['business_email']
             saveBusiness = Businesses(business_name=business_name,business_email=business_email)
             saveBusiness.save()
-            return redirect(index)
+            return redirect(neighbourhood)
     else:
-        form = BusinessForm()
-    return render(request,'blueprint/business.html',{'form':form})
+        form2 = BusinessForm()
+    return render(request,'blueprint/business.html',{'form2':form2})
 
     @classmethod
     def search_by_business_name(cls,search_term):
@@ -116,3 +121,18 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'blueprint/search.html',{"message":message})
+
+@login_required(login_url='/accounts/login/')
+def post(request):
+    current_user = request.user
+    posts = Post.objects.all()
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = current_user
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request,'blueprint/post.html',{'form':form,'posts':posts})
